32ed01080f64242aeaa48b09e19f17abdba6ad8c
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-09-11 22:14:01 +0400

    second commit

--- remember.js
+++
@@ -1,8 +0,0 @@
-// compound component pattern
-// render props pattern
-// use form hook
-const rect = e.target.closest("button").getBoundingClientRect(); // to find position where you are clicking
-const [searchParams, setSearchParams] = useSearchParams(); // to get paramteres from url and update em
-queryClient.invalidateQueries(); // to refetch data whenever maybe async function succesfully updated something in on serverside
-
-// sorting, authentication, compount component, pagination, modalwindow.
