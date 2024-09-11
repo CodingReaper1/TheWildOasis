f483abd1a6e899899e282aeabbabda4f3de13177
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-09-11 21:39:07 +0400

    first commit

---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { deleteBooking } from "../../services/apiBookings";
+
+export function useDeleteBooking() {
+  const queryClient = useQueryClient();
+
+  const { mutate: delBooking, isLoading: isDeleting } = useMutation({
+    mutationFn: (bookingId) => deleteBooking(bookingId),
+
+    onSuccess: () => {
+      toast.success(`Booking successfully deleted`);
+      queryClient.invalidateQueries({ active: true });
+    },
+
+    onError: () => toast.error("There was an error while deleting booking"),
+  });
+
+  return { delBooking, isDeleting };
+}
